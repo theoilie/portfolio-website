d98cb5b42564a0bdb5f8cb9626ec46263d9ba03a
Theo Ilie <theoilie.ti@gmail.com>
2022-11-09 23:25:30 -0500

    Update reference and LinkedIn badge

--- src/app/components/references/reference.interface.ts
+++ src/app/components/references/reference.interface.ts
@@ -7,6 +7,12 @@ export interface Reference {
 
 export const REFERENCES: Reference[] = [
   {
+    name: 'Chris Kohut',
+    title: 'Software Development Manager @ Amazon',
+    recommendationText: `It's rare to find someone like Theo. I had the pleasure of working as his manager at Amazon and I still can't believe that he turned one of his first ramp-up projects into a larger opportunity to test a performance improvement on the most critical and complicated UX component in our experience. He was always looking to make the codebase better in some way and he had such a great bias for action. He also had a positive impact on the team culture. Any manager would be lucky to have Theo.`,
+    imageUri: 'assets/chris-kohut.jpeg',
+  },
+  {
     name: 'Harly Ramsey',
     title: 'Associate Professor of Technical Communication Practice @ USC',
     recommendationText: `Theo worked for me in multiple capacities, and I have been consistently impressed: he has my highest possible recommendation.  I first met Theo as his professor over 2 years ago, I then hired him for a student-facing role, and I later enlisted his website development abilities for a project spanning 3 semesters.
@@ -14,26 +20,26 @@ export const REFERENCES: Reference[] = [
     <br /><br />I was so impressed with Theo that I hired him to be part of USC’s first podcast coaching group (a role similar to a Teaching Assistant). He shined on this team as well: he handled the ambiguity of defining and implementing a new program, skillfully mentored students with humility and empathy, and was an effective member of his coaching group.
     <br /><br />During this time I also hired Theo to create a student-facing website and was consistently pleased with the outcome, communication, and pace of progress he delivered while juggling his competing responsibilities as a podcasting coach and student. This project involved technologies Theo had not previously used, and he eagerly embraced the challenge of learning everything necessary to build my database, back-end, and front-end with a framework unfamiliar to him at the time — and to provide hosting and everything else involved with maintaining the website in accordance with my desired requirements. My requirements were at times nebulous, and Theo did an excellent job of helping me analyze the trade-offs of various approaches and of simplifying technical concepts for me.
     <br /><br />My colleagues, students, employees, and I loved working with Theo. His positive attitude, witty sense of humor, and work ethic are all missed since he has moved on—he has my absolute highest recommendation.`,
-    imageUri: 'assets/harly-ramsey.jpeg'
+    imageUri: 'assets/harly-ramsey.jpeg',
   },
   {
     name: 'Jesse Parisian',
-    title: 'Senior Software Developer @ Cross River',
+    title: 'Software Engineering Team Lead @ Cross River',
     recommendationText: `I am a Senior Software Developer and Team Lead at Vista and was Theo's direct manager over the course of his internship.
     <br /><br />I was very impressed with Theo during his time at Vista; he learned new concepts very quickly and was able to demonstrate a high level of understanding in his work. He got up to speed very quickly and completed his internship project ahead of schedule. A core component of his internship was to develop a new piece of functionality (a full feature) in our web based Staff Scheduling application called Time Card Manager. His project involved working on the front end of the application which was written in Angular 7. The project also involved working on the back end which included SQL Server, Entity Framework and .NET Web API. He also wrote unit tests around the functionality as part of the project. Our team uses a combination of Git and TFS for source control so Theo had some good exposure to both. Theo was comfortable working independently, but wasn't afraid to reach out to the team whenever he had questions or needed assistance.
     <br /><br />Theo was great to work with and everyone on the team enjoyed having him around. He did an excellent job and has a bright future ahead. We'd be happy to work with Theo again.`,
-    imageUri: 'assets/jesse-parisian.jpeg'
+    imageUri: 'assets/jesse-parisian.jpeg',
   },
   {
     name: 'Manipushpak Gupta',
-    title: 'Software Engineer @ Microsoft',
+    title: 'Software Engineer II @ Microsoft',
     recommendationText: `I’ve known Theo for almost a year now and if there’s one thing that I know about him, it’s his competency and dedication. I lived with him over the summer and can vouch for his ability to be consistent. I saw him wake up at the same early time on days when he wasn’t required to, just to work on his projects. We would also have long conversations about technology and startups and I could judge he has a great passion for it during our conversations. He’s also a self-starter. He works on technical projects - like his personal website - on the side when he has time, and recently started to improve his coding skills by doing coding challenges daily. Based on his dedication and competency, Theo would be a great asset and I would recommend working with him to anyone.`,
-    imageUri: 'assets/mani-gupta.jpeg'
+    imageUri: 'assets/mani-gupta.jpeg',
   },
   {
     name: 'Nate Potter',
-    title: 'Software Engineer II @ Duolingo',
+    title: 'Senior Software Engineer @ Duolingo',
     recommendationText: `Theo was an intern in my group at Intel's Open Source Technology Center as a high school student in the summer of 2015. He was a quick learner and valuable to the team, catching on quickly to working in open source and fundamentals in software development for an Infrastructure-as-a-Service project. By the end of his internship, he had gone through the process of community contribution in OpenStack and had multiple patches merged upstream in its Identity project, Keystone.`,
-    imageUri: 'assets/nate-potter.jpeg'
-  }
+    imageUri: 'assets/nate-potter.jpeg',
+  },
 ];
